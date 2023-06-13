ea745f483f622ed4596ddebdf0e1cd4ba1d9db5d
Dennis Ploeger <dennis.ploeger@kps.com>
2023-06-13 14:35:27 +0200

    chore: fixed integration test

--- test/integration/monitors/puppet/manifests/site.pp
+++ test/integration/monitors/puppet/manifests/site.pp
@@ -1,19 +1,3 @@
-$url = $facts['os']['family'] ? {
-  'Debian' => "https://files.blueocean-net.de/repositories/xymon-deb ${facts['os']['distro']['codename']} main",
-  'RedHat' => "https://files.blueocean-net.de/repositories/xymon-rpm/centos/${facts['os']['release']['major']}",
-  'Suse'   => "https://files.blueocean-net.de/repositories/xymon-rpm/sles/${facts['os']['release']['major']}",
-  default  => ''
-}
-
-$gpg_url = $facts['os']['family'] ? {
-  'Debian' => 'https://files.blueocean-net.de/gpg.key',
-  default  => undef
-}
-
-if ($url == '') {
-  fail("OS family ${facts['os']['family']} is not supported")
-}
-
 # Disable modifying configuration to not irritate Kitchen
 
 class {
@@ -24,12 +8,10 @@ class {
 
 class {
   'xymon::client':
-    repository_url => $url,
-    xymon_server   => '127.0.0.1',
-    gpg_url        => $gpg_url,
-    gpg_id         => '6688A3782BBFE5A4',
-    config_file    => '/etc/xymon/config',
-    monitors       => {
+    manage_repository => false,
+    xymon_server      => '127.0.0.1',
+    config_file       => '/etc/xymon/config',
+    monitors          => {
       'test'  => {
         script_source => 'puppet:///modules/files/testscript.sh',
         files         => {
